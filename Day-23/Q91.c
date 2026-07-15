017ff094c1e58682e693a75b6edc8c0c67e82eb8
Manish Kumar <manish.kumar2025@glbajajgroup.org>
2026-07-15 12:29:51 +0530

    Day-23 assignment is uploded

---
+++ Day-23/Q91.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    char str1[100], str2[100];
+    int i, j, temp;
+
+    printf("Enter first string: ");
+    gets(str1);
+
+    printf("Enter second string: ");
+    gets(str2);
+
+    for(i = 0; str1[i] != '\0'; i++) {
+        for(j = i + 1; str1[j] != '\0'; j++) {
+            if(str1[i] > str1[j]) {
+                temp = str1[i];
+                str1[i] = str1[j];
+                str1[j] = temp;
+            }
+        }
+    }
+
+    for(i = 0; str2[i] != '\0'; i++) {
+        for(j = i + 1; str2[j] != '\0'; j++) {
+            if(str2[i] > str2[j]) {
+                temp = str2[i];
+                str2[i] = str2[j];
+                str2[j] = temp;
+            }
+        }
+    }
+
+    i = 0;
+    while(str1[i] == str2[i] && str1[i] != '\0')
+        i++;
+
+    if(str1[i] == str2[i])
+        printf("Anagram Strings");
+    else
+        printf("Not Anagram Strings");
+
+    return 0;
+}
